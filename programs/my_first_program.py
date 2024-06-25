77935f0517d3769331f83c2ba42d9fb71b4a3c62
gitsofaryan <aryan.jain.csbs22@ggits.net>
2024-06-25 14:53:24 +0000

    my new nada program

--- programs/my_first_program.py
+++ programs/my_first_program.py
@@ -1,12 +1,21 @@
 from nada_dsl import *
 
-
 def nada_main():
     party1 = Party(name="Party1")
     my_int1 = SecretInteger(Input(name="my_int1", party=party1))
     my_int2 = SecretInteger(Input(name="my_int2", party=party1))
 
-    # write the computation for your program here - use my_int1 and my_int2 as inputs
-    # make sure you change the output below to be your new output
+    # Secure comparison
+    is_greater = my_int1 > my_int2
+    is_equal = my_int1 == my_int2
+    is_less = my_int1 < my_int2
 
-    return [Output(my_int1, "my_output", party1)]
+    # Create outputs for the comparison results
+    output_greater = Output(is_greater, "is_greater", party1)
+    output_equal = Output(is_equal, "is_equal", party1)
+    output_less = Output(is_less, "is_less", party1)
+
+    return [output_greater, output_equal, output_less]
+
+# Create the program with the new secure comparison logic
+nada_main()
